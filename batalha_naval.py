b507ba9a7636a5c26c89a4e91ad8eeedb4f57d9f
Mateus Weiss <mateuswmedeiros@gmail.com>
2026-05-31 02:02:10 -0300

    Primeira versão de posicionamento de barcos, ainda sem validação

--- batalha_naval.py
+++ batalha_naval.py
@@ -1,6 +1,21 @@
 import os
+from random import randint
+from time import sleep
 
-tabuleiroVazio = [
+tabuleiroFeedbackJogador = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+]
+
+tabuleiroFeedbackComputador = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -14,29 +29,52 @@ tabuleiroVazio = [
 ]
 
 tabuleiroJogador = [
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]
+    ['@',1,2,3,4,5,6,7,8,9,10],
+    ['A',0,0,0,0,0,0,0,0,0,0],
+    ['B',0,0,0,0,0,0,0,0,0,0],
+    ['C',0,0,0,0,0,0,0,0,0,0],
+    ['D',0,0,0,0,0,0,0,0,0,0],
+    ['E',0,0,0,0,0,0,0,0,0,0],
+    ['F',0,0,0,0,0,0,0,0,0,0],
+    ['G',0,0,0,0,0,0,0,0,0,0],
+    ['H',0,0,0,0,0,0,0,0,0,0],
+    ['I',0,0,0,0,0,0,0,0,0,0],
+    ['J',0,0,0,0,0,0,0,0,0,0]
 ]
 
 tabuleiroComputador = [
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]
+    ['@',1,2,3,4,5,6,7,8,9,10],
+    ['A',0,0,0,0,0,0,0,0,0,0],
+    ['B',0,0,0,0,0,0,0,0,0,0],
+    ['C',0,0,0,0,0,0,0,0,0,0],
+    ['D',0,0,0,0,0,0,0,0,0,0],
+    ['E',0,0,0,0,0,0,0,0,0,0],
+    ['F',0,0,0,0,0,0,0,0,0,0],
+    ['G',0,0,0,0,0,0,0,0,0,0],
+    ['H',0,0,0,0,0,0,0,0,0,0],
+    ['I',0,0,0,0,0,0,0,0,0,0],
+    ['J',0,0,0,0,0,0,0,0,0,0]
 ]
+
+tamanhoBarco = 5
+letrasLinhas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+for i in range (5):
+    for c in range (11):
+        print (tabuleiroJogador[c])
+
+    letraRow = input(f"Digite a linha de onde quer colocar o barco de tamanho {tamanhoBarco}: ")
+    if letraRow in letrasLinhas:
+        rowBarco = letrasLinhas.index(letraRow) + 1
+
+    columBarco = int(input(f"Digite a coluna de onde quer colocar o barco de tamanho {tamanhoBarco}: "))
+    orientacaoBarco = input(f"Digite a orientação do barco V ou H: ")
+    for j in range(tamanhoBarco):
+        tabuleiroJogador[rowBarco] [columBarco] = '■'
+        if orientacaoBarco == 'V':
+            rowBarco += 1
+        else:
+            columBarcoBarco += 1
+    tamanhoBarco -= 1
 
 os.system('cls' if os.name == 'nt' else 'clear')
